13375b28c4e01cf9a49c9c1333057379b79255d1
Mohamed Vakkayil <mohamedvakkayil@live.in>
2021-12-24 18:31:34 +0530

    adding requirements2

--- vakkayil/settings.py
+++ vakkayil/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-hmy1x0@+m1z)%cnklk3ibk-^(pn@avi!zcg_c*#4cviz#20d6c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
